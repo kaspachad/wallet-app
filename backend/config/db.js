c1354685b5044473cf7637e81744c2ac66f9bb1c
kaspachad <singletoken123@gmail.com>
2025-04-02 12:24:57 -0400

    Fixed config/db.js to use .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,17 @@
-
-// backend/config/db.js
-
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'kaspa',
-  password: 'kaspa',
-  database: 'kaspa',
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
-module.exports = db;
+module.exports = pool.promise(); // <-- So we can use async/await
 
